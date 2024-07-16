aa35fc21aaf4fda2a5bd74f081e7d2cde55e7b6c
Paulo <paulorenatorf@hotmail.com>
2024-07-16 19:16:57 -0300

    Added find all pagination

--- src/main/java/com/card/game/repository/CardRepository.java
+++ src/main/java/com/card/game/repository/CardRepository.java
@@ -1,7 +1,9 @@
 package com.card.game.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.card.game.model.Card;
@@ -9,4 +11,6 @@ import com.card.game.model.Card;
 public interface CardRepository extends JpaRepository<Card, Long>{
 
     Optional<Card> findByTitle(String title);
+
+    List<Card> findAllByTitle(String title, Pageable pageable);
 }
