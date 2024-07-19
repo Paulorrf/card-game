c47c665657cdce6f8d4c0857b375dc6dbdeffdd9
Paulo <paulorenatorf@hotmail.com>
2024-07-19 19:44:20 -0300

    fixed create card method

--- src/main/java/com/card/game/controller/CardController.java
+++ src/main/java/com/card/game/controller/CardController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,8 @@ public class CardController {
     CardService cardService;
 
     @PostMapping()
-    public ResponseEntity<CardDTO> createCard(CardDTO cardDTO) {
+    public ResponseEntity<CardDTO> createCard(@RequestBody CardDTO cardDTO) {
+        System.out.println(cardDTO.toString());
         var card = cardService.createCard(cardDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(card);
     }
